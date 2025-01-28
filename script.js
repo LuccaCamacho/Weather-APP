2b11f1e1e22825502c8dc419ca11b4d90122e2ed
LuccaCamacho <101644352+LuccaCamacho@users.noreply.github.com>
2025-01-28 17:20:21 -0300

    script.js

--- script.js
+++ script.js
@@ -14,47 +14,48 @@ buttonElement.addEventListener('click', () => {
     const apiKey = '1764e33a8aaf41c2904134738252801';
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&lang=pt`;
 
+
     // Fazendo a requisição
     fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Cidade não encontrada!');
-        }
-        return response.json();
-    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Cidade não encontrada!');
+            }
+            return response.json();
+        })
 
-    .then(data => {
-        const { location, current } = data;
+        .then(data => {
+            const { location, current } = data;
 
-        // Tornando a div visível após o clique
-        weatherInfoElement.style.display = 'block';
+            // Tornando a div visível após o clique
+            weatherInfoElement.style.display = 'block';
 
-        weatherInfoElement.innerHTML = `
-            <h2>Clima em ${location.name}, ${location.region} - ${location.country}</h2>
-            <p><strong>Temperatura:</strong> ${current.temp_c}°C</p>
-            <p><strong>Condição:</strong> ${current.condition.text}</p>
-            <img src="${current.condition.icon}" alt="Ícone do clima"></img>
-            <p><strong>Umidade:</strong> ${current.humidity}%</p>
-            <p><strong>Vento:</strong> ${current.wind_kph} km/h, direção: ${current.wind_dir}</p>
-            <p><strong>Sensação Térmica:</strong> ${current.feelslike_c}°C</p>
-            <p><strong>Precipitação:</strong> ${current.precip_mm} mm</p>
-        `;
+            weatherInfoElement.innerHTML = `
+                <h2>Clima em ${location.name}, ${location.region} - ${location.country}</h2>
+                <p><strong>Temperatura:</strong> ${current.temp_c}°C</p>
+                <p><strong>Condição:</strong> ${current.condition.text}</p>
+                <img src="${current.condition.icon}" alt="Ícone do clima"></img>
+                <p><strong>Umidade:</strong> ${current.humidity}%</p>
+                <p><strong>Vento:</strong> ${current.wind_kph} km/h, direção: ${current.wind_dir}</p>
+                <p><strong>Sensação Térmica:</strong> ${current.feelslike_c}°C</p>
+                <p><strong>Precipitação:</strong> ${current.precip_mm} mm</p>
+            `;
 
-        // Rolando suavemente para a div com as informações do clima
-        weatherInfoElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            // Rolando suavemente para a div com as informações do clima
+            weatherInfoElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
-        // Exibindo o botão "Voltar" após sucesso da requisição
-        backUpButton.style.display = 'block';
-    })
+            // Exibindo o botão "Voltar" após sucesso da requisição
+            backUpButton.style.display = 'block';
+        })
 
-    .catch(error => {
-        // Exibindo o erro caso a requisição falhe
-        weatherInfoElement.style.display = 'block'; 
-        weatherInfoElement.innerHTML = `<p style="color: red;">Erro: ${error.message}</p>`;
+        .catch(error => {
+            // Exibindo o erro caso a requisição falhe
+            weatherInfoElement.style.display = 'block';
+            weatherInfoElement.innerHTML = `<p style="color: red;">Erro: ${error.message}</p>`;
 
-        // Exibindo o botão "Voltar" em caso de erro também
-        backUpButton.style.display = 'block';
-    });
+            // Exibindo o botão "Voltar" em caso de erro também
+            backUpButton.style.display = 'block';
+        });
 });
 
 // Botão para voltar ao topo
